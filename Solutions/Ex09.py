b6b3363a01c25392cf3dbdc1e950925709c8224d
Lídia Carvalho <lidiafcarvalho@outlook.com>
2024-10-21 09:59:18 -0300

    att

--- Solutions/Ex09.py
+++ Solutions/Ex09.py
@@ -1,6 +1,5 @@
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a**2 + b**2 = c**2
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
-from math import *
 
 # c = root of (a**2 + b**2)
 # b = root of (c**2 - a**2)
